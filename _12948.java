fe0292b2079f96b771d3cc72c9876e22260b547b
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-02-21 16:14:51 +0900

    [Lv.1] 핸드폰 번호 가리기

---
+++ _12948.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        for(; i<phone_number.length()-4; i++)
+            sb.append('*');
+        for(; i<phone_number.length(); i++)
+            sb.append(phone_number.charAt(i));
+        return sb.toString();
+    }
+}
